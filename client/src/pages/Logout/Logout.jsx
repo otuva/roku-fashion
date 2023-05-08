39d4da367b427f10cffc7dacf654035aa9c49713
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 21:15:45 +0300

    add jsdoc for logout

--- client/src/pages/Logout/Logout.jsx
+++ client/src/pages/Logout/Logout.jsx
@@ -2,6 +2,11 @@ import {useEffect} from "react";
 import Cookies from "universal-cookie";
 import {CircularProgress} from "@mui/material";
 
+/**
+ A React component that logs the user out by removing the JWT token from the cookies and redirecting to the home page.
+ @function
+ @returns {JSX.Element} - The rendered component, which displays a circular progress indicator while the logout process is in progress.
+ */
 const Logout = () => {
     useEffect(() => {
         (async () => {
