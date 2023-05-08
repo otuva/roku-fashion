d3abbe331623e49ab6a9bb7098f64cb01c4f5f8a
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 16:07:16 +0300

    add home page & first component)

---
+++ src/pages/Home/Intro.js
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import {styled} from "@mui/material/styles";
+import ArrowDownward from "@mui/icons-material/ArrowDownward";
+import {Typography, Button} from "@mui/material";
+
+const backgroundImage =
+    'https://unsplash.com/photos/xXJ6utyoSw0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTd8fGNsb3RoaW5nJTIwZGVzaWduZXJ8ZW58MHx8fHwxNjgzMjg2OTgx&force=true&w=2400';
+
+const Background = styled(Box)({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    zIndex: -2,
+});
+
+const IntroRoot = styled('section')(({theme}) => ({
+    color: theme.palette.common.white,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+        height: '90vh',
+        minHeight: 500,
+        maxHeight: 1300,
+    }
+}));
+
+const Intro = () => (
+    <IntroRoot>
+        <Container sx={{
+            mt: 3,
+            mb: 14,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}>
+            {/* Increase the network loading priority of the background image. */}
+            <img
+                style={{display: 'none'}}
+                src={backgroundImage}
+                alt="increase priority"
+            />
+            <Typography align="center" variant="h2" marked="center">
+                Tailored just for you...
+            </Typography>
+            <Typography
+                align="center"
+                variant="h5"
+                sx={{mb: 4, mt: {xs: 4, sm: 10}}}
+            >
+                Skilled team of designers and craftsmen work closely with you
+                to create a garment that fits perfectly and reflects your individuality
+            </Typography>
+            <Button
+                // component={Link}
+                variant="contained"
+                size="large"
+                to={'/sign-up'}
+                sx={{minWidth: 200}}
+            >
+                Join Now
+            </Button>
+            <Typography variant="body2" sx={{mt: 2}}>
+                Invite only
+            </Typography>
+
+            <Box
+                sx={{
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                    backgroundColor: 'common.black',
+                    opacity: 0.5,
+                    zIndex: -1,
+                }}
+            />
+
+            <Background sx={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundPosition: 'center',
+            }}/>
+
+            <ArrowDownward sx={{position: 'absolute', bottom: 32}}/>
+        </Container>
+    </IntroRoot>
+);
+export default Intro
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Intro from './Intro';
+
+function Home() {
+    return (
+        <React.Fragment>
+            <Intro/>
+        </React.Fragment>
+    );
+}
+
+export default Home
