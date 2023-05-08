675ed26698ce757ed48bfca68ce80929ee5da717
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 17:37:55 +0300

    add appform component for form data

---
+++ client/src/components/form/AppForm.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Paper from '../ui/Paper';
+
+function AppForm(props) {
+    const {children} = props;
+
+    return (
+        <Container maxWidth="sm">
+            {/*<Box sx={{mt: 7, mb: 12, py: {xs: 4, md: 8}, px: {xs: 3, md: 6}}}>*/}
+            <Box sx={{mt: 7, mb: 12}}>
+                {/*<Paper*/}
+                {/*    background="dark"*/}
+                {/*    sx={{py: {xs: 4, md: 8}, px: {xs: 3, md: 6}}}*/}
+                {/*>*/}
+                {/*    */}
+                {/*</Paper>*/}
+
+                {children}
+            </Box>
+        </Container>
+    );
+}
+
+AppForm.propTypes = {
+    children: PropTypes.node,
+};
+
+export default AppForm;
