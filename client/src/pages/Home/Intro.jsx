133cda5baa76b69ce8656b89331dd5f73e9e9d39
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 19:21:47 +0300

    home page, intro component: add link to button

--- client/src/pages/Home/Intro.jsx
+++ client/src/pages/Home/Intro.jsx
@@ -5,6 +5,7 @@ import {styled} from "@mui/material/styles";
 import ArrowDownward from "@mui/icons-material/ArrowDownward";
 import {Button} from "@mui/material";
 import RokuTypography from '../../components/ui/RokuTypography';
+import {Link} from "react-router-dom";
 
 const backgroundImage =
     'https://unsplash.com/photos/xXJ6utyoSw0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTd8fGNsb3RoaW5nJTIwZGVzaWduZXJ8ZW58MHx8fHwxNjgzMjg2OTgx&force=true&w=2400';
@@ -32,65 +33,67 @@ const IntroRoot = styled('section')(({theme}) => ({
     }
 }));
 
-const Intro = () => (
-    <IntroRoot>
-        <Container sx={{
-            mt: 3,
-            mb: 14,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }}>
-            {/* Increase the network loading priority of the background image. */}
-            <img
-                style={{display: 'none'}}
-                src={backgroundImage}
-                alt="increase priority"
-            />
-            <RokuTypography align="center" variant="h2" marked="center">
-                Tailored just for you...
-            </RokuTypography>
-            <RokuTypography
-                align="center"
-                variant="h5"
-                sx={{mb: 4, mt: {xs: 4, sm: 10}}}
-            >
-                Skilled team of designers and craftsmen work closely with you
-                to create a garment that fits perfectly and reflects your individuality
-            </RokuTypography>
-            <Button
-                // component={Link}
-                variant="contained"
-                size="large"
-                to={'/sign-up'}
-                sx={{minWidth: 200}}
-            >
-                Join Now
-            </Button>
-            <RokuTypography variant="body2" sx={{mt: 2}}>
-                Invite only
-            </RokuTypography>
+const Intro = () => {
+    return (
+        <IntroRoot>
+            <Container sx={{
+                mt: 3,
+                mb: 14,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}>
+                {/* Increase the network loading priority of the background image. */}
+                <img
+                    style={{display: 'none'}}
+                    src={backgroundImage}
+                    alt="increase priority"
+                />
+                <RokuTypography align="center" variant="h2" marked="center">
+                    Tailored just for you...
+                </RokuTypography>
+                <RokuTypography
+                    align="center"
+                    variant="h5"
+                    sx={{mb: 4, mt: {xs: 4, sm: 10}}}
+                >
+                    Skilled team of designers and craftsmen work closely with you
+                    to create a garment that fits perfectly and reflects your individuality
+                </RokuTypography>
+                <Button
+                    component={Link}
+                    variant="contained"
+                    size="large"
+                    to={'/sign-up'}
+                    sx={{minWidth: 200}}
+                >
+                    Join Now
+                </Button>
+                <RokuTypography variant="body2" sx={{mt: 2}}>
+                    Invite only
+                </RokuTypography>
 
-            <Box
-                sx={{
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                    backgroundColor: 'common.black',
-                    opacity: 0.5,
-                    zIndex: -1,
-                }}
-            />
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0,
+                        backgroundColor: 'common.black',
+                        opacity: 0.5,
+                        zIndex: -1,
+                    }}
+                />
 
-            <Background sx={{
-                backgroundImage: `url(${backgroundImage})`,
-                backgroundPosition: 'center',
-            }}/>
+                <Background sx={{
+                    backgroundImage: `url(${backgroundImage})`,
+                    backgroundPosition: 'center',
+                }}/>
 
-            <ArrowDownward sx={{position: 'absolute', bottom: 32}}/>
-        </Container>
-    </IntroRoot>
-);
+                <ArrowDownward sx={{position: 'absolute', bottom: 32}}/>
+            </Container>
+        </IntroRoot>
+    );
+}
 export default Intro
