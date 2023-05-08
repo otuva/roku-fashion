0c9221cdcc4396ecf209078439e4f9369c09bedd
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 17:06:38 +0300

    new page: solutions

---
+++ src/pages/Solutions/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Categories from './Categories';
+import HowItWorks from './HowItWorks';
+const Solutions = () => {
+    return (
+        <React.Fragment>
+            <Categories/>
+            <HowItWorks/>
+        </React.Fragment>
+    );
+}
+
+export default Solutions
