f8d6370921c5d8728141461f4a0d310ea0a169f5
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 16:37:47 +0300

    add react-router to base app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-// import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import Home from "./pages/Home";
 
 function App() {
     return (
         <React.Fragment>
-            <Home/>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
         </React.Fragment>
     );
 }
