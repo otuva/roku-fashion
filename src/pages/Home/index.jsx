5382db01ad9253fb67c30c98b4f467dfe13d2690
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 16:46:13 +0300

    render gethelp on home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 
 import Intro from './Intro';
 import IconText from "./IconText";
+import GetHelp from "./GetHelp";
 
 function Home() {
     return (
         <React.Fragment>
             <Intro/>
             <IconText/>
+            <GetHelp/>
         </React.Fragment>
     );
 }
