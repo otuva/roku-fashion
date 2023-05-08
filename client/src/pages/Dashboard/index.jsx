eb1fe16ac74ca911920bd99bf9082e5baa7ce8cf
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 19:22:29 +0300

    add (admin) dashboard

---
+++ client/src/pages/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import TemporaryDrawer from './TemporaryDrawer';
+import Content from './Content';
+
+const Dashboard = () => (
+    <React.Fragment>
+        <TemporaryDrawer/>
+        <Content/>
+    </React.Fragment>
+);
+export default Dashboard
