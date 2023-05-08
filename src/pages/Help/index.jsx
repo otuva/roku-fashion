d8107c4af7add7348f38a3cc95843c2521352977
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-08 17:14:12 +0300

    help page: add page itself, render components

---
+++ src/pages/Help/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FAQ from "./FAQ";
+import ContactUs from "./ContactUs";
+
+
+const Help = () => {
+    return (
+        <React.Fragment>
+            <FAQ/>
+            <ContactUs/>
+        </React.Fragment>
+    );
+}
+
+export default Help;
