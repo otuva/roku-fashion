3ce38a6a2ceb2b4338ed66af3cc6d3c5158f700a
Onur Alp AKIN <onralpakin@gmail.com>
2023-05-11 15:19:45 +0300

    change route function param and return types

--- src/components/routes/Private.jsx
+++ src/components/routes/Private.jsx
@@ -5,8 +5,8 @@ import {AuthContext} from "../../context/AuthContext";
 
 /**
  A Higher-Order Component that conditionally renders a given component if the user is authenticated, otherwise redirects to the sign-in page.
- @param {React.ComponentType} Component - The component to render if the user is authenticated
- @returns {React.ComponentType} - A new component that conditionally renders the given component or redirects to the sign-in page
+ @param {JSX.Element} Component - The component to render if the user is authenticated
+ @returns {JSX.Element} - A new component that conditionally renders the given component or redirects to the sign-in page
  */
 const Private = (Component) => {
     const [isLoggedIn] = useContext(AuthContext);
--- src/components/routes/PublicOnly.jsx
+++ src/components/routes/PublicOnly.jsx
@@ -4,8 +4,8 @@ import {AuthContext} from "../../context/AuthContext";
 
 /**
  A Higher-Order Component (HOC) that conditionally renders a given component if the user is not authenticated, otherwise redirects to the home page.
- @param {React.ComponentType} Component - The component to render if the user is not authenticated
- @returns {React.ComponentType} - A new component that conditionally renders the given component or redirects to the home page
+ @param {JSX.Element} Component - The component to render if the user is not authenticated
+ @returns {JSX.Element} - A new component that conditionally renders the given component or redirects to the home page
  */
 const PublicOnly = (Component) => {
     const [isLoggedIn] = useContext(AuthContext);
